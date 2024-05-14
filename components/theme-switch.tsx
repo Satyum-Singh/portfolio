455ea10f234d9eb2690500f16538b687d573e6f7
Satyum <satyumspathania@gmail.com>
2024-05-14 13:17:23 +0530

    working on changing colors for dark theme.

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -12,22 +12,30 @@ export default function ThemeSwitch() {
     if (theme === "light") {
       setTheme("dark");
       window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
     } else {
       setTheme("light");
       window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
-    if (localTheme) setTheme(localTheme);
-    else if(window.matchMedia("(prefers-color-scheme:dark)").matches) setTheme('dark');
-    else setTheme('light');
+    if (localTheme) {
+      setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    } else setTheme("light");
   }, []);
 
   return (
     <button
-      className="fixed bottom-14 right-14 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black/20 border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all "
+      className="fixed dark:bg-gray-950 dark:border-white/20 bottom-14 right-14 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black/20 border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all "
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun size={24} /> : <BsMoon size={24} />}

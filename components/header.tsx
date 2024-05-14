b305af511b38d3eeaf540aeef385735264a3828c
Satyum <satyumspathania@gmail.com>
2024-05-14 13:24:13 +0530

    header dark theme done.

--- components/header.tsx
+++ components/header.tsx
@@ -9,12 +9,12 @@ import { useActiveSection } from "@/context/active-section-context";
 
 export default function Header() {
   const { active, setActive } = useActiveSection();
-  const {timeofLastClick, setTimeofLastClick} = useActiveSection();
+  const { timeofLastClick, setTimeofLastClick } = useActiveSection();
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2
+        className="fixed top-0 left-1/2 -translate-x-1/2 dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75
          h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
@@ -35,19 +35,20 @@ export default function Header() {
               <Link
                 href={link.hash}
                 className={clsx(
-                  "flex w-full items-center justify-center p-3 hover:text-gray-950 transition-colors duration-500 ease-in-out",
+                  "flex dark:text-gray-500 dark:hover:text-gray-300 w-full items-center justify-center p-3 hover:text-gray-950 transition-colors duration-500 ease-in-out",
                   {
-                    "text-gray-950": active === link.name,
+                    "text-gray-950 dark:text-gray-200": active === link.name,
                   }
                 )}
-                onClick={()=> {
-                  setActive(link.name)
-                  setTimeofLastClick(Date.now())
+                onClick={() => {
+                  setActive(link.name);
+                  setTimeofLastClick(Date.now());
                 }}
               >
                 {link.name}
                 {link.name === active && (
-                  <motion.span className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                     layoutId="active"
                     transition={{
                       type: "spring",

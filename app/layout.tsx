bd9d4b5ed9d3b89944189717411c42c45e8ee9b5
Satyum <satyumspathania@gmail.com>
2024-05-18 09:13:46 +0530

    well the theme is ready for deployment.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,18 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // This is comment is being only for theme check
     <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className} bg-gray-50 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 text-gray-950 relative h-max pt-28 sm:pt-36`}
       >
         <div
-          className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem]
-          sm:w-[68.75rem] dark:bg-[#946263]
+          className="bg-[#fbe2e3] -rotate-45 absolute top-[35rem] right-[5rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem]
+          sm:w-[68.75rem] dark:bg-gradient-to-br from-[#fc00ff] to-[#00dbde]
         "
         ></div>
         <div
-          className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem]
-          sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]
+          className="bg-[#dbd7fb] rotate-45 absolute top-[10rem] left-[15rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem]
+          sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-gradient-to-tr from-[#8A2387] via-[#E94057] to-[#F27121]
         "
         ></div>
         <ThemeContextProvider>

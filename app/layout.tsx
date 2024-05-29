b85b75e59140dcb347f6b125ee6b80ef5d1667c0
Satyum <satyumspathania@gmail.com>
2024-05-29 09:26:00 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
